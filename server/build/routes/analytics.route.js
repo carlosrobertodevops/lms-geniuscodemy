843ad3999b504e77f8ae4abd196d5adf2852fed4
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-07-10 09:46:25 -0300

    build server

---
+++ server/build/routes/analytics.route.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const analytics_controller_1 = require("../controllers/analytics.controller");
+const analyticsRouter = express_1.default.Router();
+// analyticsRouter.get("/get-users-analytics", isAutheticated,authorizeRoles("admin"), getUsersAnalytics);
+// analyticsRouter.get("/get-orders-analytics", isAutheticated,authorizeRoles("admin"), getOrderAnalytics);
+// analyticsRouter.get("/get-courses-analytics", isAutheticated,authorizeRoles("admin"), getCoursesAnalytics);
+analyticsRouter.get("/get-users-analytics", analytics_controller_1.getUsersAnalytics);
+analyticsRouter.get("/get-orders-analytics", analytics_controller_1.getOrderAnalytics);
+analyticsRouter.get("/get-courses-analytics", analytics_controller_1.getCoursesAnalytics);
+exports.default = analyticsRouter;
